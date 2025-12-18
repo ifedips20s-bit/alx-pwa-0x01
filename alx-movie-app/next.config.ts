b54ce8a0340fa0ceebdb1fac5c805cec0999389a
ifedips20s <ifedips20s@gmail.com>
2025-12-18 11:47:54 +0100

    Configure PWA using next-pwa

---
+++ alx-movie-app/next.config.ts
@@ -0,0 +1,15 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+import type { NextConfig } from "next";
+
+const withPWA = withPWAInit({
+  dest: "public",
+});
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["m.media-amazon.com"],
+  },
+};
+
+export default withPWA(nextConfig);
